5323533324322dac78875541cbe909b9b514e4ba
Skeletron72 <andrewche2003@gmail.com>
2026-01-23 21:49:21 +0300

    Configure Netlify

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,13 @@ import './index.css'
 import "nes.css/css/nes.min.css";
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+} else {
+  console.error("Critical Error: Root element not found!");
+}
